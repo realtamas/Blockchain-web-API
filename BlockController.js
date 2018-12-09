f725d88a5a13a42a21af23339982d8055f460331
tamasdinh <tamas.dinh@gmail.com>
2018-12-09 16:54:26 +0100

    b

--- BlockController.js
+++ BlockController.js
@@ -1,5 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
 const BlockClass = require('./Block.js');
+const BlockChain = require('./BlockChain.js')
 
 /**
  * Controller Definition to encapsulate routes to work with blocks
@@ -12,7 +13,7 @@ class BlockController {
      */
     constructor(app) {
         this.app = app;
-        this.blocks = [];
+        this.blockChain = new BlockChain.Blockchain();
         this.initializeMockData();
         this.getBlockByIndex();
         this.postNewBlock();
@@ -23,9 +24,14 @@ class BlockController {
      */
     getBlockByIndex() {
         let self = this;
-        this.app.get("/api/block/:index", (req, res) => {
-            res.status(200).json(self.blocks[req.params.index]);
-            res.end();
+        this.app.get("/api/block/:blockHeight", (req, res) => {
+            return self.blockChain.getBlock(req.params.blockHeight).then(result => {
+                res.set({'Connection': 'close'});
+                res.status(200).json(result);
+                res.end();
+                }, error => {
+                    res.status(404).send('\nBlock not found!');
+            });
         });
     }
 
@@ -33,21 +39,37 @@ class BlockController {
      * Implement a POST Endpoint to add a new Block, url: "/api/block"
      */
     postNewBlock() {
+        let self = this;
         this.app.post("/api/block", (req, res) => {
-            // Add your code here
+            if (req.query/*.length > 0 && typeof req.query === String*/ ) {
+                return self.blockChain.addBlock(new BlockClass.Block(req.query)).then(result => {
+                    res.set({
+                        'Connection': 'close',
+                        'Content-Type': 'application/json'
+                    });
+                    res.send(req.query);
+                    res.status(201).json(result);
+                    res.end();
+                }, error => {
+                    res.status(500).send('Uknown error occurred on server side. Please retry later.')
+                })
+            } else {
+                res.status(403).send('\nBlock data must not be empty and should be a string - please resend request with desired block data included in string format.\n')
+            }
         });
     }
 
     /**
      * Help method to inizialized Mock dataset, adds 10 test blocks to the blocks array
      */
-    initializeMockData() {
-        if(this.blocks.length === 0){
-            for (let index = 0; index < 10; index++) {
+    async initializeMockData() {
+        let blockHeight = await this.blockChain.getBlockHeight();
+        if(blockHeight === 0){
+            for (let index = 1; index < 10; index++) {
                 let blockAux = new BlockClass.Block(`Test Data #${index}`);
                 blockAux.height = index;
                 blockAux.hash = SHA256(JSON.stringify(blockAux)).toString();
-                this.blocks.push(blockAux);
+                await this.blockChain.addBlock(blockAux);
             }
         }
     }
